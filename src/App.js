394c867b32f3978b051d0ae75c4e9d1549b27190
Kevin Hilario <kevinhilario.t@gmail.com>
2022-01-16 15:35:24 -0500

    [modificado] práctica de ejercicios, [agregado] estilos de app

--- src/App.js
+++ src/App.js
@@ -1,7 +1,42 @@
-function App() {
+import './App.css';
+
+function Bandas(props) {
   return (
     <div>
-      Hola mundo
+      El cantante de <b className='nombre--banda'>{props.banda}</b>
+       es: <b>{props.cantante} </b> 
+      y su tema más famoso es: <b>{props.tema}</b>
+    </div>
+  );
+}
+
+function App() {
+  const zen = ['ZEN', 'Jhovan Tomasevich', 'Quédate'];
+  const nox = ['NoRecomendable', 'Edson lara', 'Todo está bien'];
+  const amen = ['Amén', 'Marcelo Motta', 'Te quiero'];
+  const nirvana = ['Nirvana', 'Kurt Cobain', 'Smells like teen spirit']
+  return (
+    <div>
+      <Bandas 
+      banda={zen[0]} 
+      cantante={zen[1]}
+      tema={zen[2]}
+      />
+      <Bandas   
+      banda={nox[0]} 
+      cantante={nox[1]}
+      tema={nox[2]}
+      />
+      <Bandas  
+      banda={amen[0]} 
+      cantante={amen[1]}
+      tema={amen[2]}
+      />
+      <Bandas 
+      banda={nirvana[0]} 
+      cantante={nirvana[1]}
+      tema={nirvana[2]}
+      />
     </div>
   );
 }
